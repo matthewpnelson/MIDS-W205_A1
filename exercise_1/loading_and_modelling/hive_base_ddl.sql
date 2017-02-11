65d374263e41290cff7fe66c32f6908be9a058d7
matthewpnelson <matthewpeternelson@gmail.com>
2017-02-11 15:48:42 -0700

    attempt 2

---
+++ exercise_1/loading_and_modelling/hive_base_ddl.sql
@@ -0,0 +1,24 @@
+
+DROP TABLE hospitals;
+
+CREATE EXTERNAL TABLE hospitals
+(
+    hospital_id string,
+    hospital_name string,
+    address string,
+    city string,
+    state string,
+    zip_code string,
+    county_name string,
+    phone_number string,
+    hospital_type string,
+    hospital_ownership string,
+    emergency_services string)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES(
+    "separatorChar" = ",",
+    "quoteChar" = "",
+    "escapeChar" = '\\'
+)
+STORED AS TEXTFILE
+LOCATION '/user/w205/hospital_compare';
