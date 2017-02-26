699ea02216540d4e1e2aef11f343fa6c0fac4936
matthewpnelson <matthewpeternelson@gmail.com>
2017-02-26 16:06:41 -0700

    Working SQL Queries

--- exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -2,27 +2,40 @@
 
 -- What hospitals are models of high-quality care? That is, which hospitals have the most consistently high scores for a variety of procedures.
 
--- Tables to use: procedures_detail, hospitals, survey_averages
+-- Tables to use: procedures_detail, hospital_edit
 
-SELECT hospitals.hospital_id, hospitals.hospital_name,
-    SUM(procedures_detail.care_score) AS Total_Score,
-    avg(procedures_detail.care_score) AS Avg_Score,
-    max(procedures_detail.care_score) - max(procedures_detail.care_score) AS Var_Score
-    count(procedures_detail.care_score) AS Count_Score
-    FROM hospitals
-    LEFT OUTER JOIN procedures_detail
-    ON hospitals.hospital_id = procedures_detail.care_hospital_id
-    --WHERE Count_Score > 5
-    GROUP BY procedures_detail.care_score
-    ORDER BY Avg_Score DESC
-    LIMIT 50;
+-- Selection Criteria:
+-- Grouping by Hospital, we want to see the highest average scores across all of their procedures.
 
+-- Also want to consider how many procedures they do, as a hospital could be very good at a small
+-- number of procedures by way of specialty. We are more interested in general excellence.
 
-SELECT max(cast('care_score' AS BIGINT))
-FROM procedures_detail
-ORDER BY score_int ASC
+-- An additional constraint is applied to keep the total variance between max and min scores
+-- to be below 20, as a very high average score doesn't mean consistency if there is at least one
+-- procedure that has been performed poorly at the hospital.
+
+-- Scores relating to the emergency department were excluded from aggregation, as they went beyond the standard 0-100
+-- scale.
+
+-- Focusing on only procedure care scores, not readmission scores as these are difficult
+-- to compare to one another because high procedure care scores are good while low
+-- readmissions scores (score / denominator) are good. A multi-filter could have occured
+-- between these two metrics, however it was decided not to pursue this for this assignment.
+
+-- Had to convert all of our number columns to INT before aggregating, used the cast() function.
+
+SELECT hospital_edit.hospital_id, hospital_edit.hospital_name,
+    SUM(cast(procedures_detail.care_score AS INT)) AS Total_Score,
+    round(avg(cast(procedures_detail.care_score AS INT)),2) AS Avg_Score,
+    max(cast(procedures_detail.care_score AS INT)) - min(cast(procedures_detail.care_score AS INT)) AS Var_Score,
+    COUNT(cast(procedures_detail.care_score AS INT)) AS Count_Score
+FROM hospital_edit
+LEFT OUTER JOIN procedures_detail
+ON hospital_edit.hospital_id = procedures_detail.care_hospital_id
+WHERE cast(procedures_detail.care_score AS INT) <= 100 -- Excludes Emergency Room Scores > 100
+GROUP BY hospital_edit.hospital_name, hospital_edit.hospital_id
+-- Restrict High Performers to those with at least more than 10 Procedures
+HAVING COUNT(cast(procedures_detail.care_score AS INT)) >= 10 AND
+    max(cast(procedures_detail.care_score AS INT)) - min(cast(procedures_detail.care_score AS INT)) <=20
+ORDER BY Avg_Score DESC
 LIMIT 10;
-
-SELECT CAST(care_score AS SMALLINT)
-FROM procedures_detail
-LIMIT 50;
--- exercise_1/transforming/transform_to_ER.sql
+++ exercise_1/transforming/transform_to_ER.sql
@@ -1,153 +1,84 @@
--- No Need to Recreate hospitals Table, can utilize the original import
--- No Need to Recreate survey_averages Table, can utilize the original import
+
+-- Drop extra ""'s from Hospital Table
+DROP TABLE hospital_edit;
+
+CREATE TABLE hospital_edit
+   AS
+SELECT substr(hospital_id, 2, length(hospital_id) - 2) AS hospital_id,
+    substr(hospital_name, 2, length(hospital_name) - 2) AS hospital_name,
+    substr(address, 2, length(address) - 2) AS address,
+    substr(city, 2, length(city) - 2) AS city,
+    substr(state, 2, length(state) - 2) AS state,
+    substr(zip_code, 2, length(zip_code) - 2) AS zip_code,
+    substr(county_name, 2, length(county_name) - 2) AS county_name,
+    substr(phone_number, 2, length(phone_number) - 2) AS phone_number,
+    substr(hospital_type, 2, length(hospital_type) - 2) AS hospital_type,
+    substr(hospital_ownership, 2, length(hospital_ownership) - 2) AS hospital_ownership,
+    substr(emergency_services, 2, length(emergency_services) - 2) AS emergency_services
+FROM hospitals;
 
 
+
+
+-- Drop extra ""'s from Survey Table and keep only final scores
+DROP TABLE survey_edit;
+
+CREATE TABLE survey_edit
+   AS
+SELECT substr(hospital_id, 2, length(hospital_id) - 2) AS hospital_id,
+    substr(hospital_name, 2, length(hospital_name) - 2) AS hospital_name,
+    substr(HCAHPS_Base_Score, 2, length(HCAHPS_Base_Score) - 2) AS HCAHPS_Base_Score,
+    substr(HCAHPS_Consistency_Score, 2, length(HCAHPS_Consistency_Score) - 2) AS HCAHPS_Consistency_Score
+FROM surveys;
+
+
+
+
+-- Combine Measures, Readmissions and Care Data together into one table (to match ER)
+-- Take this opportunity to remove extra quotes from all data by using substr()
+-- Build a temporary table to combine procedures&care tables, then join this temporary
+-- table with readmissions. Not sure if I can join 3 tables together in one call?
+-- Couldn't get that to work.
 
 DROP TABLE procedures_detail_temp;
 
 CREATE TABLE procedures_detail_temp
-    STORED AS TEXTFILE
+    -- STORED AS TEXTFILE
     -- LOCATION '/user/w205/hospital_compare/readmissions'
    AS
-SELECT procedures.*,
-   care.hospital_id AS care_hospital_id,
-   care.condition AS care_condition,
-   care.score AS care_score,
-   care.sample AS care_sample,
-   care.footnote AS care_footnote
+SELECT
+    substr(procedures.procedure_id, 2, length(procedures.procedure_id) - 2) AS procedure_id,
+    substr(procedures.measure_name, 2, length(procedures.measure_name) - 2) AS measure_name,
+    substr(procedures.measure_start_quarter, 2, length(procedures.measure_start_quarter) - 2) AS measure_start_quarter,
+    substr(procedures.measure_start, 2, length(procedures.measure_start) - 2) AS measure_start,
+    substr(procedures.measure_end_quarter, 2, length(procedures.measure_end_quarter) - 2) AS measure_end_quarter,
+    substr(procedures.measure_end, 2, length(procedures.measure_end) - 2) AS measure_end,
+    substr(care.hospital_id, 2, length(care.hospital_id) - 2) AS care_hospital_id,
+    substr(care.condition, 2, length(care.condition) - 2) AS care_condition,
+    substr(care.score, 2, length(care.score) - 2) AS care_score,
+    substr(care.sample, 2, length(care.sample) - 2) AS care_sample,
+    substr(care.footnote, 2, length(care.footnote) - 2) AS care_footnote
 FROM procedures
 LEFT OUTER JOIN care
 ON procedures.procedure_id = care.procedure_id;
 
+
+
 DROP TABLE procedures_detail;
 
 CREATE TABLE procedures_detail
-    STORED AS TEXTFILE
-    -- LOCATION '/user/w205/hospital_compare/readmissions'
    AS
 SELECT procedures_detail_temp.*,
-   readmissions.hospital_id AS readmissions_hospital_id,
-   readmissions.condition AS readmissions_condition,
-   readmissions.national_comparison AS readmissions_national_comparison,
-   readmissions.denominator AS readmissions_denominator,
-   readmissions.score AS readmissions_score,
-   readmissions.lower_est AS readmissions_lower_est,
-   readmissions.higher_est AS readmissions_higher_est,
-   readmissions.footnote AS readmissions_footnote
+    substr(readmissions.hospital_id, 2, length(readmissions.hospital_id) - 2) AS readmissions_hospital_id,
+    substr(readmissions.condition, 2, length(readmissions.condition) - 2) AS readmissions_condition,
+    substr(readmissions.national_comparison, 2, length(readmissions.national_comparison) - 2) AS readmissions_national_comparison,
+    substr(readmissions.denominator, 2, length(readmissions.denominator) - 2) AS readmissions_denominator,
+    substr(readmissions.score, 2, length(readmissions.score) - 2) AS readmissions_score,
+    substr(readmissions.lower_est, 2, length(readmissions.lower_est) - 2) AS readmissions_lower_est,
+    substr(readmissions.higher_est, 2, length(readmissions.higher_est) - 2) AS readmissions_higher_est,
+    substr(readmissions.footnote, 2, length(readmissions.footnote) - 2) AS readmissions_footnote
 FROM procedures_detail_temp
 LEFT OUTER JOIN readmissions
 ON procedures_detail_temp.procedure_id = readmissions.procedure_id;
 
 DROP TABLE procedures_detail_temp;
-
--- DROP TABLE procedures_detail_temp1;
---
--- CREATE TABLE procedures_detail_temp1(procedures.*, care_hospital_id, care_condition, care_score, care_sample, care_footnote)
---     ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
---     WITH SERDEPROPERTIES (
---         "separatorChar" = ",",
---         "quoteChar" = "'",
---         "escapeChar" = "\\"
---     )
---     STORED AS TEXTFILE
---     LOCATION'/user/w205/hospital_compare/'
---    AS
--- SELECT procedures.*,
---    care.hospital_id AS care_hospital_id,
---    care.condition AS care_condition,
---    care.score AS care_score,
---    care.sample AS care_sample,
---    care.footnote AS care_footnote
--- FROM procedures_detail_temp1
--- LEFT OUTER JOIN care
--- ON procedures.procedure_id = care.procedure_id;
---
---
-
-
--- Create Procedures (Measures) Table
--- DROP TABLE procedures_detail_temp1;
---
--- CREATE TABLE procedures_detail_temp1
--- AS SELECT *
--- FROM procedures;
---
--- -- Complete a left outer join with the effective care table in order to bring
--- -- in all information relating to procedures and the hospitals which perform
--- -- them
---
--- -- Working code, duplicates the table but ads joined data.
--- -- Could not get INSERT INTO SELECT procedures_detail working...
--- DROP TABLE procedures_detail_temp2;
---
--- CREATE TABLE procedures_detail_temp2 AS
--- SELECT procedures_detail_temp1.*,
---     care.hospital_id AS care_hospital_id,
---     care.condition AS care_condition,
---     care.score AS care_score,
---     care.sample AS care_sample,
---     care.footnote AS care_footnote
--- FROM procedures_detail_temp1
--- LEFT OUTER JOIN care
--- ON procedures_detail_temp1.procedure_id = care.procedure_id;
---
--- DROP TABLE procedures_detail_temp1;
---
--- -- Couldn't get this working...
--- --INSERT INTO procedures_detail (care_hospital_id, care_condition, care_score, care_sample, care_footnote)
--- --SELECT care.hospital_id AS care_hospital_id,
--- --    care.condition AS care_condition,
--- --    care.score AS care_score,
--- --    care.sample AS care_sample,
--- --    care.footnote AS care_footnote
--- --FROM procedures_detail
--- --LEFT OUTER JOIN care
--- --ON procedures_detail.procedure_id = care.procedure_id;
---
---
--- -- Complete a left outer join with the readmissions table in order to bring
--- -- in all information relating to procedures and the hospitals which perform
--- -- them
--- DROP TABLE procedures_detail;
---
--- CREATE TABLE procedures_detail AS
--- SELECT procedures_detail_temp2.*,
---     readmissions.hospital_id AS readmissions_hospital_id,
---     readmissions.condition AS readmissions_condition,
---     readmissions.national_comparison AS readmissions_national_comparison,
---     readmissions.denominator AS readmissions_denominator,
---     readmissions.score AS readmissions_score,
---     readmissions.lower_est AS readmissions_lower_est,
---     readmissions.higher_est AS readmissions_higher_est,
---     readmissions.footnote AS readmissions_footnote
--- FROM procedures_detail_temp2
--- LEFT OUTER JOIN readmissions
--- ON procedures_detail_temp2.procedure_id = readmissions.procedure_id;
---
--- DROP TABLE procedures_detail_temp2;
---
--- SELECT measure_start
--- FROM procedures
--- WHERE procedure_id = '"MORT_30_AMI"'
--- LIMIT 50;
---
--- SELECT procedures_detail_temp2.procedure_id,
---     readmissions.procedure_id
--- FROM procedures_detail_temp2
--- LEFT OUTER JOIN readmissions
--- ON procedures_detail_temp2.procedure_id = readmissions.procedure_id
--- LIMIT 50;
-
-
--- to query column names
--- SHOW COLUMNS FROM care
-
--- to query you need to use double quotes!?!
--- SELECT measure_start, measure_
--- FROM procedures_detail
--- WHERE procedure_id = '"AMI_7a"'
--- LIMIT 10;
-
--- change data type?
---ALTER TABLE care
---ALTER COLUMN score INTEGER
